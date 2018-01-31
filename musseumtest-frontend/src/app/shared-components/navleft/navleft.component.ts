04c8d58eb9346ad266a4045a1c5c42188e679e94
Jairo95 <jairotft@gmail.com>
2018-01-30 20:19:38 -0500

    Diseño base

---
+++ musseumtest-frontend/src/app/shared-components/navleft/navleft.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navleft',
+  templateUrl: './navleft.component.html',
+  styleUrls: ['./navleft.component.css']
+})
+export class NavleftComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- musseumtest-frontend/src/app/shared-components/shared-components.module.ts
+++ musseumtest-frontend/src/app/shared-components/shared-components.module.ts
@@ -5,24 +5,29 @@ import { Quizformat1Component } from './quizformat1/quizformat1.component';
 import { Quizformat2Component } from './quizformat2/quizformat2.component';
 import { Quizformat3Component } from './quizformat3/quizformat3.component';
 import { Quizformat4Component } from './quizformat4/quizformat4.component';
+import { NavleftComponent } from './navleft/navleft.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     NavtopComponent,
     Quizformat1Component,
     Quizformat2Component,
     Quizformat3Component,
-    Quizformat4Component
+    Quizformat4Component,
+    NavleftComponent
   ],
   exports: [
     NavtopComponent,
     Quizformat1Component,
     Quizformat2Component,
     Quizformat3Component,
-    Quizformat4Component
+    Quizformat4Component,
+    NavleftComponent
   ]
 })
 export class SharedComponentsModule { }
