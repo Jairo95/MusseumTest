9b4ad6005eb75c1670c12cf15331ec9613d21ba0
Jairo95 <jairotft@gmail.com>
2018-01-30 22:42:11 -0500

    NgIf for questions

--- musseumtest-frontend/src/app/teacher/teacher/teacher.component.ts
+++ musseumtest-frontend/src/app/teacher/teacher/teacher.component.ts
@@ -7,21 +7,23 @@ import {SelectItem} from 'primeng/api';
   styleUrls: ['./teacher.component.css']
 })
 export class TeacherComponent implements OnInit {
-  selectedtypeQuestion: string;
+  selectedtypeQuestion: SelectItem;
   typeQuestions: SelectItem[];
 
 
   constructor() {
-  }
-
-  ngOnInit() {
     this.typeQuestions = [
+      {label: 'Selecciones tipo de pregunta', value: '0'},
       {label: 'Pregunta simpe', value: '1'},
       {label: 'Pregunta Verdadero o falso', value: '2'},
       {label: 'pregunta selección única respuesta respuesta', value: '3'},
       {label: 'pregunta selección múltiple', value: '4'},
     ];
+    this.selectedtypeQuestion = this.typeQuestions[0];
     console.log(this.typeQuestions);
+  }
+
+  ngOnInit() {
   }
 
 }
