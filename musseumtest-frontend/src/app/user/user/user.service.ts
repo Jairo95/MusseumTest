03657d91e3921416ba648ef373515624a7995a42
wilgue10 <wilmer.guevara@outlook.es>
2018-02-19 14:04:33 -0500

    servicegetquestion

--- musseumtest-frontend/src/app/user/user/user.service.ts
+++ musseumtest-frontend/src/app/user/user/user.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+
+import { Uservalidate } from '../../models/uservalidate';
 
 @Injectable()
 export class UserService {
-
-  constructor() { }
-
+  urlBase = 'http://localhost:50446';
+  constructor(   private http: HttpClient ) { }
+  getQuestions(classromm:number ): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      })
+    };
+    const url = this.urlBase + '/api/Logins'
+    const body = new URLSearchParams();
+    body.set('Username', email);
+    body.set('Password', password);
+    return this.http.post(url, body.toString(), httpOptions );
+  }
 }
